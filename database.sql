7ae573399bbbb78cdfa4fa34d709482455d8d50a
Mohammed MAADELALLOUI <med.maadellaoui@gmail.com>
2019-12-04 23:16:13 +0100

    database structure

---
+++ database.sql
@@ -0,0 +1,21 @@
+CREATE TABLE package (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    package_name TEXT NOT NULL,
+    absolute_path TEXT NOT NULL
+);
+
+CREATE TABLE class (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    class_name TEXT NOT NULL,
+    absolute_path TEXT NOT NULL,
+    package_id INTEGER NOT NULL,
+    FOREIGN KEY(package_id) REFERENCES package(id)
+);
+
+CREATE TABLE link (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    class_src INTEGER NOT NULL,
+    class_dst INTEGER NOT NULL,
+    FOREIGN KEY(class_src) REFERENCES class(id),
+    FOREIGN KEY(class_dst) REFERENCES class(id)
+);
