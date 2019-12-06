0d2de721e8e973fc542618a9e46d47dd24319c9c
Mohammed MAADELALLOUI <med.maadellaoui@gmail.com>
2019-12-07 00:51:42 +0100

    update database constraints

--- database.sql
+++ database.sql
@@ -1,12 +1,12 @@
 CREATE TABLE package (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    package_name TEXT NOT NULL,
+    package_name TEXT NOT NULL UNIQUE,
     absolute_path TEXT NOT NULL
 );
 
 CREATE TABLE class (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    class_name TEXT NOT NULL,
+    class_name TEXT NOT NULL UNIQUE,
     absolute_path TEXT NOT NULL,
     package_id INTEGER NOT NULL,
     FOREIGN KEY(package_id) REFERENCES package(id)
@@ -18,4 +18,5 @@ CREATE TABLE link (
     class_dst INTEGER NOT NULL,
     FOREIGN KEY(class_src) REFERENCES class(id),
     FOREIGN KEY(class_dst) REFERENCES class(id)
+    CONSTRAINT unq UNIQUE (class_src, class_dst)
 );
